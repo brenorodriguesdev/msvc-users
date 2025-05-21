d80dc864b9f9ce092e3d1ad4f65fb42211fb2bd9
brenorodriguesdev <brenorodriguesdev@gmail.com>
2025-05-21 17:39:37 -0300

    feat: implements bcrypt hasher compare

--- infra/bcrypt.go
+++ infra/bcrypt.go
@@ -18,3 +18,15 @@ func (h *BcryptHasher) Hash(password string) (string, error) {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	return string(bytes), err
 }
+
+type BcryptHasherCompare struct{}
+
+func NewBcryptHasherCompare() contracts.HasherCompare {
+	return &BcryptHasherCompare{}
+}
+
+var _ contracts.HasherCompare = (*BcryptHasherCompare)(nil)
+
+func (h *BcryptHasherCompare) Compare(password, hash string) (bool, error) {
+	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password)) == nil, nil
+}
